958e00a900dc9ae4c114b87e93995e19a767a2d0
Benaiah Mischenko <benaiah@mischenko.com>
2021-05-25 12:12:29 -0700

    Interpolate previously-hardcoded values from env variables

---
+++ build.sh
@@ -0,0 +1,17 @@
+if [ -z "$SLEEPY_DURATION" ]; then
+  SLEEPY_DURATION="100"
+fi
+
+sleep $SLEEPY_DURATION
+
+if [ -z "$NETLIFY_API_HOST" ]; then
+  NETLIFY_API_HOST="api.netlify.com"
+fi
+
+if [ -z "$BUILD_HOOK_ID" ]; then
+  BUILD_HOOK_ID="NO_BUILD_HOOK_ID_PROVIDED"
+fi
+
+cat index.html.template | \
+  sed "s/NETLIFY_API_HOST/$NETLIFY_API_HOST/g;s/BUILD_HOOK_ID/$BUILD_HOOK_ID/g" | \
+  tee index.html
